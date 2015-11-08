b52729acc32a5abd0510c2dd24b8522de42b71cf
levin <3259963@qq.com>
2015-11-08 17:35:08 +0800

    overLongWeight.py

--- overLongWeight.py
+++ overLongWeight.py
@@ -1,87 +1,131 @@
-# -*- coding: utf8 -*-
-def isOverWeight(grossWeights=1, pieces=1, threshold=80):
-    '''判断是否超重 true=是 false=否
-    grossWeights=毛重 KG
-    pieces=件数
-    threshold=阈值 KG
+# -*- coding: utf-8 -*-
+
+
+def isOverWeight(grossWeights=1, pieces=1, overWeightThreshold=80):
+    '''判断是否超重
+    @param grossWeights->float 毛重 unit: kg
+    @param pieces->int 件数
+    @param overWeightThreshold->float 超重阈值 unit: kg
+    @return->boolean true: 超重, false: 不超重
     '''
+    assert pieces > 0, "件数必须大于0"
+    assert grossWeights >= 0, "毛重必须大于等于0"
     average = grossWeights / pieces
-    return average > threshold
+    return average > overWeightThreshold
 
 
 def isOverLong(longs=0, widths=0, highs=0,
-               singleThreshold=160, perimeterThreshold=300):
-    '''判断是否超长 true=是 false=否
-    longs=长 cm, widths=宽 cm, highs=高 cm
-    singleThreshold=单边长度阈值 cm
-    perimeterThreshold=周长阈值 cm
+               overLongSingleThreshold=160, overLongPerimeterThreshold=300):
+    '''判断是否超长
+    @param longs->float unit: cm
+    @param widths->float unit: cm
+    @param highs->float unit: cm
+    @param overLongSingleThreshold->float 超长单边阈值 unit: cm
+    @param overLongPerimeterThreshold->float 超长周长阈值 unit: cm
+    @return->boolean true: 超长, false: 不超长
     '''
     perimeter = longs + widths + highs
-    return longs > singleThreshold or widths > singleThreshold \
-        or highs > singleThreshold or perimeter > perimeterThreshold
+    return longs > overLongSingleThreshold or \
+        widths > overLongSingleThreshold or \
+        highs > overLongSingleThreshold or \
+        perimeter > overLongPerimeterThreshold
 
 
 def _getRate(prodCode, regionType):
     '''获取抛重比
+    @param prodCode->str 产品编码
+    @param regionType->str 区域类型编码 R20101(省内件)/...
+    @return->float 轻抛比 6000/9000/12000
     '''
     return 9000
 
 
 def _getPrice(prodCode):
     '''获取产品定价
-    prodCode=产品编码
+    @param prodCode->str 产品编码
+    @return->float
     '''
     return 1
 
 
 def _getOfferPrice(prodCode):
     '''获取产品报价
-    prodCode=产品编码
+    @param prodCode->float 产品编码
+    @return->float
     '''
     return 1
 
 
 def _getMiniChargedPrice(prodCode):
     '''获取产品定价的最低收费
-    prodCode=产品编码
+    @param prodCode->str 产品编码
+    @return->float
     '''
     return 20
 
 
 def _getMiniChargedOfferPrice(prodCode):
     '''获取产品报价的最低收费
-    prodCode=产品编码
+    @param prodCode->str 产品编码
+    @return->float
     '''
     return 20
 
 
+def _getOverWeightThreshold():
+    '''获取超重阈值
+    @return->float unit: kg
+    '''
+    return 80
+
+
+def _getOverLongSingleThreshold():
+    '''获取超长单边阈值
+    @return->float unit: cm
+    '''
+    return 160
+
+
+def _getOverLongPerimeterThreshold():
+    '''获取超长周长阈值
+    @return->float unit: cm
+    '''
+    return 300
+
+
 def calculateChargedWeight(longs=0, widths=0, highs=0,
-                           rate=6000, grossWeight=0):
+                           rate=6000, grossWeights=0):
     '''计算计费重量
     @param longs->float unit:cm
     @param widths->float unit:cm
     @param highs->float unit:cm
     @param rate->int 轻抛比 (6000/9000/12000)
-    @param grossWeight->float 毛重
+    @param grossWeights->float 毛重
+    @return->float
     '''
     chargeWeight = (longs * widths * highs) / rate
-    return chargeWeight if chargeWeight > grossWeight else grossWeight
+    return chargeWeight if chargeWeight > grossWeights else grossWeights
 
 
 def calculateOverLongChargedWeight(longs=0, widths=0, highs=0,
-                                   rate=6000, grossWeight=0):
+                                   rate=6000, grossWeights=0):
     '''计算超长计费重量
-    longs=长 cm, widths=宽 cm, highs=高 cm
-    rate=轻抛比, grossWeight=毛重
+    @param longs->float unit: cm
+    @param widths->float unit: cm
+    @param highs->float unit: cm
+    @param rate->float 轻抛比
+    @param grossWeights->float 毛重
+    @return->float
     '''
-    return calculateChargedWeight(longs, widths, highs, rate, grossWeight)
+    return calculateChargedWeight(longs, widths, highs, rate, grossWeights)
 
 
 def calculateOverWeightPrice(chargedWeight, unitPrice, miniChargedPrice):
     '''计算超重价格
-    chargedWeight=计费重量
-    unitPrice=单价 元/KG
-    miniChargedPrice=最低收费
+    @param chargedWeight->float 计费重量 unit: kg
+    @param unitPrice->float 单价 unit: 元/kg
+    @para miniChargedPrice->float 最低收费 unit: 元
+    @return->float
     '''
     price = chargedWeight * unitPrice
     return price if price > miniChargedPrice else miniChargedPrice
@@ -90,9 +134,10 @@ def calculateOverWeightPrice(chargedWeight, unitPrice, miniChargedPrice):
 def calculateOverLongPrice(overLongChargedWeight,
                            unitPrice, miniChargedPrice):
     '''计算超长价格
-    overLongChargedWeight=超长计费重量
-    unitPrice=单价 元/KG
-    miniChargedPrice=最低收费
+    @param overLongChargedWeight->float 超长计费重量
+    @param unitPrice->float 单价 unit: 元/kg
+    @para miniChargedPrice->float 最低收费 unit: 元
+    @return->float
     '''
     return calculateOverWeightPrice(overLongChargedWeight,
                                     unitPrice, miniChargedPrice)
@@ -106,7 +151,7 @@ if __name__ == '__main__':
     productCode = 'VA0006'
     regionType = 'R20101'  # 区域类型
     grossWeights = 89.61
-    pieces = 1
+    pieces = 5
 
     # 单价
     unitPrice = _getOfferPrice(productCode)
@@ -120,13 +165,23 @@ if __name__ == '__main__':
     # 超长计费重量
     overLongChargedWeight = calculateOverLongChargedWeight(
         longs, widths, highs, rate, grossWeights)
+    # 超重阈值
+    overWeightThreshold = _getOverWeightThreshold()
+    # 超长单边阈值
+    overLongSingleThreshold = _getOverLongSingleThreshold()
+    # 超长周长阈值
+    overLongPerimeterThreshold = _getOverLongPerimeterThreshold()
 
-    if isOverWeight(grossWeights, pieces, threshold=80):
+    if isOverWeight(grossWeights, pieces, overWeightThreshold):
         overWeightPrice = calculateOverWeightPrice(
             chargedWeight, unitPrice, miniChargedPrice)
-    else:
+    elif isOverLong(longs, widths, highs,
+                    overLongSingleThreshold,
+                    overLongPerimeterThreshold):
         overLongPrice = calculateOverLongPrice(
             overLongChargedWeight, unitPrice, miniChargedPrice)
+    else:
+        pass
 
     print("overWeightPrice = ", overWeightPrice)
     print("overLongPrice = ", overLongPrice)
