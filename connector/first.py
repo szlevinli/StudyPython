66b90073de981f4c6de23e58f89be6810558bb4b
levin <3259963@qq.com>
2015-11-12 18:04:40 +0800

    modify connnector files

--- connector/first.py
+++ connector/first.py
@@ -3,9 +3,23 @@
 import mysql.connector
 
 optionFiles = 'connnector_config.ini'
+query = (" select"
+         "    t.cust_code"
+         "    ,t.start_dt"
+         " from"
+         "    tmp_03 t"
+         " limit %s, %s")
 try:
     cnx = mysql.connector.connect(option_files=optionFiles)
+    cursor = cnx.cursor()
+    cursor.execute(query, (0, 10))
+
+    for cust_code, start_dt in cursor:
+        print("cust_code=", cust_code, sep='{', end='} ')
+        print("start_dt=", start_dt, sep='{', end='}\n')
+
 except mysql.connector.Error as err:
     print(err)
 else:
+    cursor.close()
     cnx.close()
