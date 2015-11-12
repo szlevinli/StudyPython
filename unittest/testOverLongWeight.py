e422b45f2f80f5b2687e97d149490e80cfdd4f04
levin <3259963@qq.com>
2015-11-12 22:24:38 +0800

    a

--- unittest/testOverLongWeight.py
+++ unittest/testOverLongWeight.py
@@ -1,11 +1,16 @@
 # -*- coding: utf8 -*-
 
 import unittest
-import overLongWeight
+from overLongWeight import *
 
-class Test(object):
-    """docstring for Test"""
-    def __init__(self, arg):
-        super(Test, self).__init__()
-        self.arg = arg
-        
+
+class TestOverLongWeight(unittest.TestCase):
+
+    def test_isOverWeight(self):
+        self.assertEqual(isOverWeight(0.2, 1, 80), False)
+        self.assertEqual(isOverWeight(1, 1, 1), False)
+        with self.assertRaises(AssertionError):
+            self.assertEqual(isOverWeight(1, 0, 1), False)
+
+if __name__ == '__main__':
+    unittest.main()
