a4b081d50815c18abf6f5c634976c0f433c4feab
levin <3259963@qq.com>
2015-11-17 08:02:13 +0800

    modify test_datahide.py

--- test_datahide.py
+++ test_datahide.py
@@ -1,12 +1,26 @@
 # -*- coding: utf8 -*-
 
+
 class DataHide():
+
     """变量前加两个__可以实现隐藏
+    实际上python会将有前缀两个下划线的变量进行“名称改编”(name mangling)
+    改编后的名字为_className__variableName
     """
-    __v = 0
-    v = 0
+    __v = 0  # 将进行name mangling, _DataHide__v
+    v = 1
+
+    def __init__(self):
+        self.s = 'sss'
+        self.__s = '__s'  # 将进行name mangling, _DataHide__s
 
 d = DataHide()
 
 print(DataHide.v)
-print(DataHide.__v)
+print(DataHide._DataHide__v)
+print(d.s)
+print(d._DataHide__s)
+# print(d.__s)
+# print error AttributeError: 'DataHide' object has no attribute '__s'
+# print(DataHide.__v)
+# print error AttributeError: 'DataHide' object has no attribute '__v'
