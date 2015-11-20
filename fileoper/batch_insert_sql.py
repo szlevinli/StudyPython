45941876333418c4201d32d1549698c8567eb470
levin <3259963@qq.com>
2015-11-20 23:05:01 +0800

    modify fileoper

--- fileoper/batch_insert_sql.py
+++ fileoper/batch_insert_sql.py
@@ -1,18 +1,41 @@
 # -*- coding: utf8 -*-
 
 
+class Timing:
+
+    def __init__(self, f):
+        self.f = f
+
+    def __call__(self, *args, **kwargs):
+        from datetime import datetime
+        start_dt = datetime.now()
+        self.f(*args, **kwargs)
+        end_dt = datetime.now()
+        speed_millis = (end_dt - start_dt).total_seconds() * 1000
+        print("call [%s] function elapsed time is [%f] milliseconds " %
+              (self.f.__name__, speed_millis))
+
+
+@Timing
 def copy(fromFile, toFile):
-    lines = 56
-    with open(fromFile, mode='r', encoding='utf-8') as r, \
+    with open(fromFile, mode='r', encoding='utf8') as r, \
             open(toFile, mode='w') as w:
-        buffers = r.readlines(lines)
-        print(type(buffers))
-        print(buffers)
-        #for line in r.readlines(100):
-            # buffers.append(line)
-        #    print(type(line))
-        #    print(line)
-        w.writelines(buffers)
+        for line in r:
+            w.writelines(line)
+
+
+@Timing
+def copy2(fromFile, toFile):
+    read_size = 1024 * 1024 * 4
+    with open(fromFile, mode='r', encoding='utf8') as r, \
+            open(toFile, mode='w') as w:
+        while True:
+            lines = r.readlines(read_size)
+            if not lines:
+                break
+            w.writelines(lines)
 
 if __name__ == '__main__':
-    copy("f.csv", "t.csv")
+    import sys
+    copy2(sys.argv[1], sys.argv[2])
+
