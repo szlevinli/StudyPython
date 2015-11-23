b4bdf66be3b63f8ed0c3ffb8b1e0b8363c1ba4b5
levin <3259963@qq.com>
2015-11-23 22:56:00 +0800

    modify fileoper to support regular express

--- fileoper/batch_insert_sql.py
+++ fileoper/batch_insert_sql.py
@@ -2,6 +2,7 @@
 
 
 from decorator.decorator import Timing
+import re
 
 
 @Timing
@@ -21,7 +22,31 @@ def copy2(fromFile, toFile):
             lines = r.readlines(read_size)
             if not lines:
                 break
+            resql(lines, 10)
             w.writelines(lines)
+
+
+@Timing
+def resql(lines, max_lines):
+    i = 0
+    j = 0
+    p = re.compile('(INSERT INTO .*\) VALUES )', re.I)
+    p2 = re.compile(';$')
+    for line in lines:
+        m = p.search(line)
+        if m is not None:
+            k = j % max_lines
+            if k == 0:
+                line = p2.sub(',', line)
+            elif k == max_lines - 1:
+                line = p.sub('', line)
+            else:
+                line = p.sub('', line)
+                line = p2.sub('', line)
+            j += 1
+        lines[i] = line
+        i += 1
+
 
 if __name__ == '__main__':
     import sys
